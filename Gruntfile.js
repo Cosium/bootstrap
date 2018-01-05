8f1d6732b0e81bdc0dcc60776a9704db4436a4a9
rgarcia <yzerno@gmail.com>
2018-01-05 19:41:30 +0100

    Disable pre commit Hookeekekkejhfbfhbfshvjwc;f"js dry

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,10 +198,7 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['before-test', 'test', 'after-test']);
 
   grunt.registerTask('enforce', `Install commit message enforce script if it doesn't exist`, function() {
-    if (!grunt.file.exists('.git/hooks/commit-msg')) {
-      grunt.file.copy('misc/validate-commit-msg.js', '.git/hooks/commit-msg');
-      require('fs').chmodSync('.git/hooks/commit-msg', '0755');
-    }
+
   });
 
   //Common ui.bootstrap module containing all modules for src and templates
